9bb6bc4b5dacc47c2f4bb56a282ac8e91945f7d8
varmoes <guillaume@alleycorpnord.com>
2025-03-22 15:41:19 -0400

    Initial commit

---
+++ delphine_vl53l4cd.ino
@@ -0,0 +1,59 @@
+#include <vl53l4cd_class.h>
+#include <Wire.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <Arduino.h>
+
+#define DEV_I2C Wire
+#define SerialPort Serial
+
+VL53L4CD sensor_vl53l4cd_sat(&DEV_I2C, -1);
+
+void setup() {
+  SerialPort.begin(115200);
+  SerialPort.println("Starting...");
+
+  // Initialize I2C bus.
+  DEV_I2C.begin();
+
+  // Configure VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.begin();
+
+  // Switch off VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.VL53L4CD_Off();
+
+  //Initialize VL53L4CD satellite component.
+  sensor_vl53l4cd_sat.InitSensor();
+
+  // Program the highest possible TimingBudget, without enabling the
+  // low power mode. This should give the best accuracy
+  sensor_vl53l4cd_sat.VL53L4CD_SetRangeTiming(200, 0);
+
+  // Start Measurements
+  sensor_vl53l4cd_sat.VL53L4CD_StartRanging();
+}
+
+void loop() {
+  uint8_t NewDataReady = 0;
+  uint16_t distance;
+  uint8_t status;
+  VL53L4CD_Result_t results;
+
+  do {
+    status = sensor_vl53l4cd_sat.VL53L4CD_CheckForDataReady(&NewDataReady);
+  } while (!NewDataReady);
+
+  if ((!status) && (NewDataReady != 0)) {
+    // (Mandatory) Clear HW interrupt to restart measurements
+    sensor_vl53l4cd_sat.VL53L4CD_ClearInterrupt();
+
+    // Read measured distance. RangeStatus = 0 means valid data
+    sensor_vl53l4cd_sat.VL53L4CD_GetResult(&results);
+    SerialPort.println(results.distance_mm);
+    delay(1000);
+  }
+}
